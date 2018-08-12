52ad1c10b23eee041e1ba2e5cea7023c05b01f7d
Barrett Langton <bdlangton@gmail.com>
2018-08-12 16:14:23 -0600

    More updates to the Highlight type.

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,5 +1,6 @@
 class HighlightsController < ApplicationController
   def new
+    @highlight = Highlight.new
   end
 
   def index
@@ -10,11 +11,35 @@ class HighlightsController < ApplicationController
     @highlight = Highlight.find(params[:id])
   end
 
+  def edit
+    @highlight = Highlight.find(params[:id])
+  end
+
+  def update
+    @highlight = Highlight.find(params[:id])
+
+    if @highlight.update(highlight_params)
+      redirect_to @highlight
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @highlight = Highlight.new(highlight_params)
 
-    @highlight.save
-    redirect_to @highlight
+    if @highlight.save
+      redirect_to @highlight
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @highlight = Highlight.find(params[:id])
+    @highlight.destroy
+
+    redirect_to highlights_path
   end
 
   private
