c14834c90b3fc9fcc336dc4df4471d935bbd331e
Barrett Langton <bdlangton@gmail.com>
2018-08-11 20:07:20 -0600

    Added some basic crud functionality.

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,4 +1,24 @@
 class HighlightsController < ApplicationController
   def new
   end
+
+  def index
+    @highlights = Highlight.all
+  end
+
+  def show
+    @highlight = Highlight.find(params[:id])
+  end
+
+  def create
+    @highlight = Highlight.new(highlight_params)
+
+    @highlight.save
+    redirect_to @highlight
+  end
+
+  private
+    def highlight_params
+      params.require(:highlight).permit(:title, :text)
+    end
 end
