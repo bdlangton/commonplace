f9458886685e81807a6cbac2d67c46fd9b70c870
Barrett Langton <bdlangton@gmail.com>
2018-08-18 12:43:03 -0600

    Added kindle email/pw to environment variables.

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -2,8 +2,8 @@ class ImportController < ApplicationController
   def import
     require '/usr/local/lib/ruby/gems/2.5.0/gems/kindle-highlights-2.0.1/lib/kindle_highlights.rb'
     kindle = KindleHighlights::Client.new(
-      email_address: "",
-      password: ""
+      email_address: ENV['COMMONPLACE_KINDLE_EMAIL'],
+      password: ENV['COMMONPLACE_KINDLE_PW']
     )
     @user = User.find(1)
     @books = kindle.books
