43b815c00ea85f3a9eaf24980f65b4e6fbae2b7f
Barrett Langton <bdlangton@gmail.com>
2018-12-26 14:23:03 -0700

    Use sixteen different colors for tag backgrounds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   $('a.favorite').bind('ajax:success', favoriteHighlight);
   $('a.publish').bind('ajax:success', publishHighlight);
 
+  // Favorite or unfavorite a highlight.
   function favoriteHighlight(event, data) {
     // TODO: Why is data null?
     if (event.detail[0].favorite) {
@@ -33,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   }
 
+  // Publish or unpublish a highlight.
   function publishHighlight(event, data) {
     // TODO: Why is data null?
     if (event.detail[0].published) {
@@ -44,6 +46,63 @@ document.addEventListener("DOMContentLoaded", function(event) {
       $('#publish-' + event.detail[0].id).attr('href', '/highlights/' + event.detail[0].id + '/publish');
     }
   }
+
+  // Update tag colors based on the first letter of each tag.
+  $('.tag.badge').each(function () {
+    var first_char = $(this).html().substring(0, 1).toLowerCase();
+    if ($.inArray(first_char, ['a', 'q']) > -1) {
+      $(this).css('background-color', 'navy');
+    }
+    else if ($.inArray(first_char, ['b', 'r']) > -1) {
+      $(this).css('background-color', 'blue');
+    }
+    else if ($.inArray(first_char, ['c', 's']) > -1) {
+      $(this).css('color', 'black');
+      $(this).css('background-color', 'aqua');
+    }
+    else if ($.inArray(first_char, ['d', 't']) > -1) {
+      $(this).css('background-color', 'teal');
+    }
+    else if ($.inArray(first_char, ['e', 'u']) > -1) {
+      $(this).css('background-color', 'olive');
+    }
+    else if ($.inArray(first_char, ['f', 'v']) > -1) {
+      $(this).css('background-color', 'green');
+    }
+    else if ($.inArray(first_char, ['g', 'w']) > -1) {
+      $(this).css('color', 'black');
+      $(this).css('background-color', 'lime');
+    }
+    else if ($.inArray(first_char, ['h', 'x']) > -1) {
+      $(this).css('color', 'black');
+      $(this).css('background-color', 'yellow');
+    }
+    else if ($.inArray(first_char, ['i', 'y']) > -1) {
+      $(this).css('background-color', 'orange');
+    }
+    else if ($.inArray(first_char, ['j', 'z']) > -1) {
+      $(this).css('background-color', 'red');
+    }
+    else if (first_char == 'k') {
+      $(this).css('background-color', 'maroon');
+    }
+    else if (first_char == 'l') {
+      $(this).css('background-color', 'fuchsia');
+    }
+    else if (first_char == 'm') {
+      $(this).css('background-color', 'purple');
+    }
+    else if (first_char == 'n') {
+      $(this).css('background-color', 'black');
+    }
+    else if (first_char == 'o') {
+      $(this).css('background-color', 'gray');
+    }
+    else if (first_char == 'p') {
+      $(this).css('color', 'black');
+      $(this).css('background-color', 'silver');
+    }
+  });
 
   // Hide flash notices after a few seconds.
   $('.flash').fadeOut(3000);
--- app/helpers/highlights_helper.rb
+++ app/helpers/highlights_helper.rb
@@ -2,7 +2,7 @@ module HighlightsHelper
   def tag_links(tags)
     tags.split(",").map{|tag|
       @tag = Tag.find_by(title: tag.strip, user: current_user)
-      link_to @tag.title, tag_path(@tag.id), class: ['badge', 'badge-pill', 'badge-success'] if @tag
+      link_to @tag.title, tag_path(@tag.id), class: ['badge', 'badge-pill', 'badge-success', 'tag'] if @tag
     }.join('')
   end
 end
