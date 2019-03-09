b51606b9e2426ffa27042342b1bdc98b1e25c1d0
Barrett Langton <barrett@langton.dev>
2019-03-09 11:01:44 -0700

    Add sources capybara tests

--- spec/features/sources_spec.rb
+++ spec/features/sources_spec.rb
@@ -7,39 +7,80 @@ include Features
 feature "sources" do
   background do
     @user1 = create(:user, email: 'user@example.com', password: '123456')
-    @source1 = create(:source, user: @user1)
+    @source1 = create(:source, user: @user1, title: 'A source')
+    @source2 = create(:source, user: @user1, source_type: 'Artist', title: 'Second source')
     @user2 = create(:user, email: 'user2@example.com', password: '123456')
-    @source2 = create(:source, user: @user2)
+    @source3 = create(:source, user: @user2)
   end
 
   scenario "adds new source" do
     sign_in_as('user@example.com')
     visit sources_path
-    pending "pending"
-    expect(1).to eq(2)
+
+    click_on 'New source'
+    fill_in 'source[title]', with: 'My source'
+    fill_in 'source[author]', with: 'Mr Author'
+    fill_in 'source[source_type]', with: 'Book'
+    fill_in 'source[notes]', with: 'My notes'
+    click_on 'Save Source'
+
+    expect(page).to have_css('h1', text: 'My source')
+    visit sources_path
+    expect(page).to have_css('table.sources td.title', text: 'My source')
   end
 
-  scenario "edit source" do
+  scenario "edits source" do
     sign_in_as('user@example.com')
     visit sources_path
-    pending "pending"
-    expect(1).to eq(2)
+
+    find('#edit-' + @source1.id.to_s).click
+    fill_in 'source[title]', with: 'Edited source'
+    fill_in 'source[notes]', with: 'Edited note'
+    click_on 'Save Source'
+
+    expect(page).to have_css('h1', text: 'Edited source')
+    expect(page).to have_css('p', text: 'Edited note')
+    visit sources_path
+    expect(page).to have_css('table.sources td.title', text: 'Edited source')
   end
 
   scenario "deletes source" do
+    pending "have to use selenium but it is running on dev, not test"
+    # Capybara.current_driver = :selenium
+    # Capybara.server = :puma
     sign_in_as('user@example.com')
     visit sources_path
-    pending "pending"
-    expect(1).to eq(2)
+
+    accept_alert do
+      find('#edit-' + @source1.id.to_s).click
+    end
   end
 
   scenario "filters source type" do
-    pending "filters source type"
-    expect(1).to eq(2)
+    sign_in_as('user@example.com')
+    visit sources_path
+
+    expect(page).to have_css('table.sources tbody tr', count: 2)
+
+    select 'Artist', from: 'Source type'
+    click_on 'Filter'
+    expect(page).to have_css('table.sources tbody tr', count: 1)
+
+    select 'Book', from: 'Source type'
+    click_on 'Filter'
+    expect(page).to have_css('table.sources tbody tr', count: 1)
   end
 
   scenario "sort by" do
-    pending "sort by"
-    expect(1).to eq(2)
+    sign_in_as('user@example.com')
+    visit sources_path
+
+    select 'Title', from: 'sort'
+    click_on 'Filter'
+    expect(page).to have_xpath('//table/tbody/tr[1]/td[@class="title"]', text: 'A source')
+
+    select 'Last Added', from: 'sort'
+    click_on 'Filter'
+    expect(page).to have_xpath('//table/tbody/tr[1]/td[@class="title"]', text: 'Second source')
   end
 end
