3a49a73d3ccf9883e1fa57af6cc2750f9a8cdf60
Barrett Langton <barrett@langton.dev>
2019-07-16 19:57:57 -0600

    Print list of authors on views

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -1,2 +1,6 @@
 module SourcesHelper
+  # Given a source, print out comma separated list of authors (links).
+  def print_authors(source)
+    source.authors.map { |author| link_to(author.name, author_path(author)) }.join(', ').html_safe
+  end
 end
