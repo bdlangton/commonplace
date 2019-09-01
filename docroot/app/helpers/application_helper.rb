37804c5ff3a98c9b33a922eb135bc183b7959bce
Barrett Langton <barrett@langton.dev>
2019-09-01 14:20:00 -0600

    Show author and source tags on the 'show' page

--- docroot/app/helpers/application_helper.rb
+++ docroot/app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
   # Given a comma separated list of tags, generate links for each tag.
   def tag_links(tags)
-    tags.split(",").map{|tag|
-      @tag = Tag.find_by(title: tag.strip, user: current_user)
-      link_to @tag.title, tag_path(@tag.id), class: ['badge', 'badge-pill', 'badge-success', 'tag'] if @tag
-    }.join('')
+    # The tags may be a comma separated list of values or an ActiveRecord
+    # CollectionProxy.
+    if tags.is_a? String
+      tags.split(",").map{|tag|
+        @tag = Tag.find_by(title: tag.strip, user: current_user)
+        link_to @tag.title, tag_path(@tag.id), class: ['badge', 'badge-pill', 'badge-success', 'tag'] if @tag
+      }.join('')
+    else
+      tags.map{|tag|
+        link_to tag.title, tag_path(tag.id), class: ['badge', 'badge-pill', 'badge-success', 'tag']
+      }.join('')
+    end
   end
 end
