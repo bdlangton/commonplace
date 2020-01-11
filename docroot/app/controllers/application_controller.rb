a220e47f7d7a56593db4643241c872d7898e7a8b
Barrett Langton <barrett@langton.dev>
2020-01-11 12:46:08 -0700

    Set Rails to API mode
    Ref: https://hashrocket.com/blog/posts/how-to-make-rails-5-api-only.

--- docroot/app/controllers/application_controller.rb
+++ docroot/app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 # Application controller.
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+class ApplicationController < ActionController::API
 end
